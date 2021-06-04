4e1231c7c8536790b29ccd11330b2e650556ba95
IsmMah <ismat.mahmood@softwire.com>
2021-06-04 22:06:36 +0100

    add knight moves

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class Knight extends Piece {
@@ -6,6 +7,34 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let availableMoves = [];
+      
+            if (location.row + 2 < 8 && location.row + 1 >= 0){
+                availableMoves.push(Square.at(location.row + 2, location.col + 1));
+            }
+            if (location.row + 2 >= 0 && location.row - 1 < 8 ){
+                availableMoves.push(Square.at(location.row + 2, location.col - 1));
+            }
+            if (location.col - 2  < 8 && location.col + 1 >= 0){
+                availableMoves.push(Square.at(location.row - 2, location.col + 1));
+            }
+            if (location.col - 2 >= 0 && location.col - 1 < 8){
+                availableMoves.push(Square.at(location.row - 2, location.col - 1));
+            }
+            if (location.row + 1 < 8 && location.row + 2 >= 0){
+                availableMoves.push(Square.at(location.row + 1, location.col + 2));
+            }
+            if (location.row + 1 >= 0 && location.row - 2 < 8 ){
+                availableMoves.push(Square.at(location.row + 1, location.col - 2));
+            }
+            if (location.col - 1  < 8 && location.col + 2 >= 0){
+                availableMoves.push(Square.at(location.row - 1, location.col + 2));
+            }
+            if (location.col - 1 >= 0 && location.col - 2 < 8){
+                availableMoves.push(Square.at(location.row - 1, location.col - 2));
+            }
+        
+        return availableMoves;
     }
 }
