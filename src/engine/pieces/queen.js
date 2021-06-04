b56ca9aa2eb584acfbc0df0cfb2a3ef6fec0b867
IsmMah <ismat.mahmood@softwire.com>
2021-06-04 19:20:03 +0100

    added queen moves

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,34 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let availableMoves = [];
+        for (let i = 1; i < 8; i++) {
+            if ((location.row + i < 8 && location.row + i >= 0) && (location.col + i < 8 && location.col + i >= 0)){
+                availableMoves.push(Square.at(location.row + i, location.col + i));
+            }
+            if ((location.row + i < 8 && location.row + i >= 0) && (location.col - i < 8 && location.col - i >= 0)){
+                availableMoves.push(Square.at(location.row + i, location.col - i));
+            }
+            if ((location.row - i < 8 && location.row - i >= 0) && (location.col +i < 8 && location.col + i >= 0)){
+                availableMoves.push(Square.at(location.row - i, location.col + i));
+            }
+            if ((location.row - i < 8 && location.row - i >= 0) && (location.col - i < 8 && location.col - i >= 0)){
+                availableMoves.push(Square.at(location.row - i, location.col - i));
+            }
+            if (location.row + i < 8 && location.row + i >= 0){
+                availableMoves.push(Square.at(location.row + i, location.col));
+            }
+            if (location.row - i >= 0 && location.row - i < 8 ){
+                availableMoves.push(Square.at(location.row - i, location.col));
+            }
+            if (location.col + i < 8 && location.col + i >= 0){
+                availableMoves.push(Square.at(location.row, location.col + i));
+            }
+            if (location.col - i >= 0 && location.col - i < 8){
+                availableMoves.push(Square.at(location.row, location.col - i));
+            }
+        }
+        return availableMoves;   
     }
 }
