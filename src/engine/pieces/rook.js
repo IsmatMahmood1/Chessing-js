6c7142d2dde671fecdb468ed95872c263c864dce
IsmMah <ismat.mahmood@softwire.com>
2021-06-04 19:05:00 +0100

    add rook moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,7 @@
+
+import Square from '../square';
 import Piece from './piece';
+
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +9,22 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let availableMoves = [];
+        for (let i = 1; i < 8; i++) {
+            if (location.row + i < 8 && location.row + i >= 0){
+                availableMoves.push(Square.at(location.row + i, location.col));
+            }
+            if (location.row - i >= 0 && location.row - i < 8 ){
+                availableMoves.push(Square.at(location.row - i, location.col));
+            }
+            if (location.col + i < 8 && location.col + i >= 0){
+                availableMoves.push(Square.at(location.row, location.col + i));
+            }
+            if (location.col - i >= 0 && location.col - i < 8){
+                availableMoves.push(Square.at(location.row, location.col - i));
+            }
+        }
+        return availableMoves;
     }
 }
